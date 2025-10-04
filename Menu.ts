d44ebd06d2ee75e4030f32d3a1d03335b0400527
davidbaarbosa <davidbaarbosa18@gmail.com>
2025-10-03 21:17:55 -0300

    .

--- Menu.ts
+++ Menu.ts
@@ -1,25 +1,77 @@
-import leia = require("readline-sync");
-import {Colors} from "./util/color";
+import readlinesync = require("readline-sync");
+import { Colors } from ".src/util/colors";
 
-let continuar: boolean = true;
+export function main() {
+
 let opcao: number;
 
-do {
+while (true) {
+
 console.log(Colors.YELLOW + "************************************");
 console.log(Colors.YELLOW +" Loja Online - Compra Facil");
 console.log(Colors.YELLOW +"************************************");
-console.log(Colors.YELLOW +"1 - Buscar Produtos");
-console.log(Colors.YELLOW +"2 - Listar favoritos");
-console.log(Colors.YELLOW +"3 - Meus Pedidos");
+console.log(Colors.YELLOW +"1 - Cadastrar Produtos");
+console.log(Colors.YELLOW +"2 - Listar Produtos");
+console.log(Colors.YELLOW +"3 - Buscar Produtos");
 console.log(Colors.YELLOW +"4 - Adicionar Produto ao Carrinho");
 console.log(Colors.YELLOW +"5 - Remover Produto do Carrinho");
-console.log(Colors.YELLOW +"6 - Finalizar Compra");
-console.log(Colors.YELLOW +"7 - Sair");
+console.log(Colors.YELLOW +"6 - Sair");
 console.log(Colors.YELLOW +"************************************");
 console.log(Colors.YELLOW +"Entre com a opção desejada: ");
 
-opcao = leia.questionInt();
+opcao = readlinesync.questionInt("");
 
-console.log(opcao);
+if (opcao == 6) {
+            console.log(colors.fg.cyanstrong,"\nCompra Fácil - tudo o que você precisa, sem complicação.");
+            sobre();
+            console.log(colors.reset, '')
+            process.exit(7);
 
-} while (continuar);
+}
+
+ switch (opcao) {
+            case 1:
+                console.log(colors.fg.cyanstrong,"\n\nCadastrar produto\n\n", colors.reset);
+
+                break;
+            case 2:
+                console.log(colors.fg.cyanstrong,"\n\nListar produtos\n\n", colors.reset);
+
+                break;
+            case 3:
+                console.log(colors.fg.cyanstrong,"\n\nBuscar Produtos\n\n", colors.reset);
+
+                break;
+            case 4:
+                console.log( colors.fg.cyanstrong,"\n\nAdicionar Produtos\n\n" , colors.reset);
+
+                break;
+            case 5:
+                console.log( colors.fg.cyanstrong,"\n\nRemover Produto\n\n" , colors.reset);
+
+                break;
+
+            default:
+                console.log( colors.fg.cyanstrong,"\nOpção Inválida!\n" , colors.reset);
+
+                break;
+        }
+    }
+
+}
+
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("David Barbosa  - dev.davidbarbosa@gmail.com");
+    console.log("https://github.com/DavidBaarbosa");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
